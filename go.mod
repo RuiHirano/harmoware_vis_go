b476b2fa7c50faa2785d421a9257a42e5d9135a6
Rui Hirano <r.hrn.0930@gmail.com>
2020-12-06 12:09:03 +0900

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RuiHirano/harmoware_vis_go
 
 go 1.13
+
+require github.com/mtfelian/golang-socketio v1.5.2 // indirect
--- harmoware_vis_go.go
+++ harmoware_vis_go.go
@@ -1,7 +1,12 @@
 package harmwoware_vis_go
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	gosocketio "github.com/mtfelian/golang-socketio"
 )
 
 type HarmowareVisGo struct{
@@ -14,9 +19,75 @@ func NewHarmowareVisGo() *HarmowareVisGo {
 	}
 }
 
-func (hv *HarmowareVisGo) RunServer(backgraund bool) {
-	if backgraund{
-		fmt.Printf("background")
+func (hv *HarmowareVisGo) RunServer(address string) {
+	ioserv := hv.runIOServer()
+	log.Printf("Running Sio Server..\n")
+	if ioserv == nil {
+		os.Exit(1)
 	}
-	fmt.Printf("foreground")
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/socket.io/", ioserv)
+	serveMux.HandleFunc("/", hv.assetsFileHandler)
+	log.Printf("Starting Harmoware VIS  Provider on %s", address)
+	err := http.ListenAndServe(address, serveMux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (hv *HarmowareVisGo) runIOServer()*gosocketio.Server {
+	
+
+	assetsDir := hv.getAssetsDir()
+	log.Println("AssetDir:", assetsDir)
+
+	server := gosocketio.NewServer()
+
+	server.On(gosocketio.OnConnection, func(c *gosocketio.Channel) {
+		log.Printf("Connected from %s as %s", c.IP(), c.Id())
+	})
+
+	server.On(gosocketio.OnDisconnection, func(c *gosocketio.Channel) {
+		log.Printf("Disconnected from %s as %s", c.IP(), c.Id())
+	})
+
+	return server
+}
+
+
+// assetsFileHandler for static Data
+func (hv *HarmowareVisGo) assetsFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet && r.Method != http.MethodHead {
+		return
+	}
+
+	file := r.URL.Path
+	//	log.Printf("Open File '%s'",file)
+	if file == "/" {
+		file = "/index.html"
+	}
+	assetsDir := hv.getAssetsDir()
+	f, err := assetsDir.Open(file)
+	if err != nil {
+		log.Printf("can't open file %s: %v\n", file, err)
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		log.Printf("can't open file %s: %v\n", file, err)
+		return
+	}
+	http.ServeContent(w, r, file, fi.ModTime(), f)
+}
+func (hv *HarmowareVisGo) getAssetsDir() http.Dir{
+	currentRoot, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	d := filepath.Join(currentRoot, "build")
+
+	assetsDir := http.Dir(d)
+	return assetsDir
 }
