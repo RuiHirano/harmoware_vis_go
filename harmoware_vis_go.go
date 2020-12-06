c7cdc919c5ff9f317266f6bf7f03b809bf05569e
Rui Hirano <r.hrn.0930@gmail.com>
2020-12-06 12:00:32 +0900

    update

--- harmoware_vis_go.go
+++ harmoware_vis_go.go
@@ -1,5 +1,22 @@
 package harmwoware_vis_go
 
-func Hello() string {
-    return "Hello"
+import (
+	"fmt"
+)
+
+type HarmowareVisGo struct{
+
+}
+
+func NewHarmowareVisGo() *HarmowareVisGo {
+    return &HarmowareVisGo{
+
+	}
+}
+
+func (hv *HarmowareVisGo) RunServer(backgraund bool) {
+	if backgraund{
+		fmt.Printf("background")
+	}
+	fmt.Printf("foreground")
 }
